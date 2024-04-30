3e77d6d87d67e992cf5e717f6b585903f314dfdd
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-04-29 20:22:21 -0400

    Complete

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,5 @@
 export default function Total({bill, tip1, tip2}) {
-    const tip = (bill) * ((tip1 + tip2) / 200);
+    const tip = (bill * (tip1 + tip2)) / 200;
     const total = bill + tip;
     return <div className="container">
         <h2>You pay ${total} (${bill} + ${tip} tip)</h2>
